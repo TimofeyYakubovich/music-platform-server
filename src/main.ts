bb30d3f785f49a4f40ad0da6959c5ae3c357dd0e
Timofey <yakubovich96@mail.ru>
2023-09-28 17:22:17 +0300

    add express

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { urlencoded, json } from 'express';
 
 const start = async () => {
     try {
         const PORT = process.env.PORT || 5000;
-        const app = await NestFactory.create(AppModule) 
+        const app = await NestFactory.create(AppModule)
+        app.use(json({ limit: '50mb' }));
+        app.use(urlencoded({ extended: true, limit: '50mb' }));
         app.enableCors() // корс что бы отправлять запросы с браузера без проблем
         await app.listen(PORT, () => console.log(`server started on port ${PORT}`))
     } catch (e) {
