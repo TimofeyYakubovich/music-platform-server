6f019c7d5e9d6fb5c5b11b69ad00624cb55c4763
Timofey <yakubovich96@mail.ru>
2023-10-03 13:30:46 +0300

    origin1

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ const start = async () => {
         app.enableCors({
             origin: 'https://music-platform-client.vercel.app', // Укажите домен, с которого разрешены запросы
             // origin: '*', // Укажите домен, с которого разрешены запросы
-            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Укажите разрешенные HTTP-методы
+            methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS', // Укажите разрешенные HTTP-методы
             credentials: true, // Разрешить передачу куки и заголовков авторизации
         }) // корс что бы отправлять запросы с браузера без проблем
         await app.listen(PORT, () => console.log(`server started on port ${PORT}`))
